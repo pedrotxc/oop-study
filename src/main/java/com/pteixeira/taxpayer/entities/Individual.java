31a936768e5aa7ca7c7c86eb2bafc1881b7a9772
Pedro Henrique Teixeira Costa <pedrotxc@gmail.com>
2025-08-26 22:01:35 -0300

    Herança e polimorfismo, projeto de tax payer

---
+++ src/main/java/com/pteixeira/taxpayer/entities/Individual.java
@@ -0,0 +1,36 @@
+package com.pteixeira.taxpayer.entities;
+
+public class Individual extends TaxPayer {
+
+    private Double healthExpenditures;
+
+    public Individual() {
+
+    }
+
+    public  Individual(String name, Double anualIncome, Double healthExpenditures) {
+        super(name, anualIncome);
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    public Double getHealthExpenditures() {
+        return healthExpenditures;
+    }
+
+    public void setHealthExpenditures(Double healthExpenditures) {
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    @Override
+    public Double tax() {
+        Double basicTax = (getAnualIncome() < 20000) ? getAnualIncome() * 0.15 : getAnualIncome() * 0.25;
+
+        basicTax -= getHealthExpenditures() * 0.5;
+
+        if (basicTax < 0) {
+            basicTax = 0.0;
+        }
+        return basicTax;
+    }
+
+}
---
+++ src/main/java/com/pteixeira/taxpayer/entities/Program.java
@@ -0,0 +1,53 @@
+package com.pteixeira.taxpayer.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<TaxPayer> list = new ArrayList<TaxPayer>();
+
+        System.out.println("Enter the number of taxpayers: ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println("Taxpayer #" + i + " data: ");
+            System.out.println("Individual or Company (i/c)? ");
+            char type = sc.next().charAt(0);
+
+            System.out.println("Name: ");
+            String name = sc.next();
+
+            System.out.println("Anual income:");
+            Double anualIncome = sc.nextDouble();
+
+            if (type == 'i') {
+                System.out.println("Health expenditures: ");
+                Double healthExpenditures = sc.nextDouble();
+                Individual individual = new Individual(name, anualIncome, healthExpenditures);
+                list.add(individual);
+            } else {
+                System.out.println("Number of employees: ");
+                Integer numberOfEmployees = sc.nextInt();
+                Company company = new Company(name, anualIncome, numberOfEmployees);
+                list.add(company);
+            }
+
+            System.out.println();
+            System.out.println("TAXES PAID: ");
+            for (TaxPayer tp : list) {
+                System.out.println(tp.getName() + ": $" + String.format("%.2f", tp.tax()));
+            }
+            double sum = 0.0;
+            for (TaxPayer tp : list) {
+                sum += tp.tax();
+            }
+            System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
+        }
+    }
+}
